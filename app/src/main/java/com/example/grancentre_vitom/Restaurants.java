39170c0fcf7802178bb0520aafd0fe1b5aac92e8
viapl <viaplanaferran@gmail.com>
2023-11-20 14:53:16 +0100

    actualizacion xml y java de restaurantes

--- app/src/main/java/com/example/grancentre_vitom/Restaurants.java
+++ app/src/main/java/com/example/grancentre_vitom/Restaurants.java
@@ -3,6 +3,8 @@ package com.example.grancentre_vitom;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class Restaurants extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class Restaurants extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurants);
+
+        Spinner spinner = findViewById(R.id.FiltreRestaurants);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this,
+                //solo cambia el array segun lo que quieras poner, esta definido en el string.xml
+                R.array.restaurantList,
+                android.R.layout.simple_spinner_item
+        );
+
     }
 }
