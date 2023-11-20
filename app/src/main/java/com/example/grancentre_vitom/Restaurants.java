53363469d6f5a7bd6dbcfe56e7aa66b627f29ea9
viapl <viaplanaferran@gmail.com>
2023-11-20 20:54:29 +0100

    actualizacion xml y java de restaurantes

--- app/src/main/java/com/example/grancentre_vitom/Restaurants.java
+++ app/src/main/java/com/example/grancentre_vitom/Restaurants.java
@@ -3,15 +3,17 @@ package com.example.grancentre_vitom;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Spinner;
 
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class Restaurants extends AppCompatActivity {
-
+ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,7 +29,7 @@ public class Restaurants extends AppCompatActivity {
         adapterSpinner.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapterSpinner);
 
-        ListView listView = findViewById(R.id.ListView);
+        listView = findViewById(R.id.ListView);
         ArrayAdapter<CharSequence> adapterList = ArrayAdapter.createFromResource(
                 this,
                 R.array.restaurantList,
@@ -36,9 +38,32 @@ public class Restaurants extends AppCompatActivity {
         listView.setAdapter(adapterList);
 
     }
+    //filtra por nombre pero falta aprender a filtrar por tipo
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        // Obtener la opción seleccionada en el Spinner
+        String categoriaSeleccionada = parent.getItemAtPosition(position).toString();
+        //obtenemos el array de restaurantes
+        String [] restaurants = getResources().getStringArray(R.array.restaurantList);
+        // Filtrar los datos en el adaptador del ListView según la categoría seleccionada
+        ArrayList<String> datosFiltrados = new ArrayList<>();
+        for (String restaurante : restaurants) {
+            if (restaurante.contains(categoriaSeleccionada)) {
+                datosFiltrados.add(restaurante);
+            }
+        }
 
-    public void onItemSelected() {
+        // Actualizar el adaptador del ListView con los datos filtrados
+        ArrayAdapter<String> adapterFiltrado = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                datosFiltrados
+        );
+        listView.setAdapter(adapterFiltrado);
+    }
 
+    //@Override
+    public void onNothingSelected(AdapterView<?> parent){
+        //no hagas nada si no hay nada seleccionado
     }
 
 }
