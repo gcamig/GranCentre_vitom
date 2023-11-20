42b4e668fa2508bbeebb4269be8d3c29a13540bf
viapl <viaplanaferran@gmail.com>
2023-11-20 15:34:08 +0100

    actualizacion xml y java de restaurantes

--- app/src/main/java/com/example/grancentre_vitom/Restaurants.java
+++ app/src/main/java/com/example/grancentre_vitom/Restaurants.java
@@ -3,8 +3,12 @@ package com.example.grancentre_vitom;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Spinner;
+
+import java.lang.reflect.Array;
 
 public class Restaurants extends AppCompatActivity {
 
@@ -14,13 +18,27 @@ public class Restaurants extends AppCompatActivity {
         setContentView(R.layout.activity_restaurants);
 
         Spinner spinner = findViewById(R.id.FiltreRestaurants);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+        ArrayAdapter<CharSequence> adapterSpinner = ArrayAdapter.createFromResource(
                 this,
                 //solo cambia el array segun lo que quieras poner, esta definido en el string.xml
-                R.array.restaurantList,
+                R.array.restaurantType,
                 android.R.layout.simple_spinner_item
         );
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
+        adapterSpinner.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapterSpinner);
+
+        ListView listView = findViewById(R.id.ListView);
+        ArrayAdapter<CharSequence> adapterList = ArrayAdapter.createFromResource(
+                this,
+                R.array.restaurantList,
+                android.R.layout.simple_list_item_1
+        );
+        listView.setAdapter(adapterList);
+
     }
+
+    public void onItemSelected() {
+
+    }
+
 }
