2d9345c8d94c11b3e635ff7040f79447d9240401
28victor <victorescos.b@gmail.com>
2023-11-24 12:14:37 +0100

    Movies Rest

--- app/src/main/java/com/example/grancentre_vitom/Movies.java
+++ app/src/main/java/com/example/grancentre_vitom/Movies.java
@@ -1,16 +1,9 @@
 package com.example.grancentre_vitom;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Parcelable;
 import android.view.View;
 import android.widget.ImageButton;
-import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class Movies extends AppCompatActivity implements View.OnClickListener {
 
