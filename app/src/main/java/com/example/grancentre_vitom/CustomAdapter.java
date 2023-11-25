880efe2787e3bde7119e55370a48afd3755cb9b9
viapl <viaplanaferran@gmail.com>
2023-11-25 16:42:48 +0100

    mejora del custom adapter

--- app/src/main/java/com/example/grancentre_vitom/CustomAdapter.java
+++ app/src/main/java/com/example/grancentre_vitom/CustomAdapter.java
@@ -24,7 +24,14 @@ public class CustomAdapter extends BaseAdapter {
     }
     @Override
     public int getCount() {
-        return listRestaurant.length;
+        int contadorNoNulos = 0;
+
+        for (int i = 0; i < listRestaurant.length; i++) {
+            if (listRestaurant[i] != null) {
+                contadorNoNulos++;
+            }
+        }
+        return contadorNoNulos;
     }
 
     @Override
